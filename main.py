2cafdb3bd933f1cf3e3cdbf29ca94975dd5ceee8
mengtorayeev-coder <mengtorayeev@gmail.com>
2026-05-12 00:25:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, ContextTypes
 
+TOKEN = "8799433110:AAFEiF8J8sxkghnafFTsBZZiLjsmHvWNKYs"
+KANAL = "@https://t.me/kinolaruztvv"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
+
+    try:
+        member = await context.bot.get_chat_member(KANAL, user_id)
+
+        if member.status in ["left", "kicked"]:
+            keyboard = [
+                [InlineKeyboardButton("📢 Kanalga obuna bo‘lish", url=f"https://t.me/{KANAL.replace('@','')}")],
+                [InlineKeyboardButton("✅ Tekshirish", callback_data="check")]
+            ]
+            await update.message.reply_text(
+                "❗ Botdan foydalanish uchun kanalga obuna bo‘ling:",
+                reply_markup=InlineKeyboardMarkup(keyboard)
+            )
+        else:
+            await update.message.reply_text("🎬 Xush kelibsiz! Kino botga kirdingiz.")
+
+    except:
+        await update.message.reply_text("Xatolik yuz berdi. Kanalni tekshiring.")
+
+app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
